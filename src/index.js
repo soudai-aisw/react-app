914f29eba3b6bc97984ea17a94e52dc1531a3cb1
soudai-aisw <79563815+soudai-aisw@users.noreply.github.com>
2023-06-21 19:28:57 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+//ReactDomを使いindexl.htmlのrootタグに対して描画
+ReactDom.render(<App />, document.getElementById("root"));
